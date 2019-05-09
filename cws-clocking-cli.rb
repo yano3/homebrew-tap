85aff674d510382b7da4d29e2878f26c2a9f9635
Teppei Yano <buckelwal@gmail.com>
2019-05-09 19:19:19 +0900

    version up cws-clocking-cli

--- cws-clocking-cli.rb
+++ cws-clocking-cli.rb
@@ -1,9 +1,9 @@
 class CwsClockingCli < Formula
-  version 'v0.1.0'
+  version 'v0.1.1'
   desc "Build okara URL with cli."
   homepage "https://github.com/yano3/cws-clocking-cli"
   url "https://github.com/yano3/cws-clocking-cli/releases/download/v0.1.0/cws-clocking-cli_darwin_amd64.zip"
-  sha256 "0d039f3aa7fd4bef86d3acf75fbc2856df7c4df107d816bc5dddd88872cdea27"
+  sha256 "c2883abffcc33fb0c8a518e11e9833110f2bd2e015ac3625116af8d2cdc35eab"
 
   def install
     bin.install 'cws-clocking-cli'
